f096bac7f92704f9b969dda49a4759d5f4163d72
Hridayy <73392185+Hridayy@users.noreply.github.com>
2021-08-17 16:05:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-//https://teachablemachine.withgoogle.com/models/GjLbSpCI6/
+
+Webcam.set({
+    width:340,
+    height:300,
+    image_format:'png',
+    png_quality:90
+    })
+    camera=document.getElementById("camera")
+    Webcam.attach("#camera")
+    
+    function take_snapshot(){
+    Webcam.snap(
+        function (data_uri){
+    document.getElementById("result").innerHTML='<img id="capture" src="'+ data_uri +'"/>'
+    }
+    )
+    }
+    
+    console.log("ml5 version:",ml5.version)
+    classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/GjLbSpCI6/model.json',modelloaded)
+    
+    function modelloaded(){
+        console.log("modelloaded")
+    }
